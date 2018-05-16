6cf79e1e46feadc9660fb31af757383ff5ac776a
Aridaman Nandan <aridaman1995@gmail.com>
2018-05-16 13:11:41 +0530

    initial

---
+++ routes/pages.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const route=express();
+
+route.get('/students',(request,response)=>{
+   response.sendFile('students.html',{root:'./public'});
+})
+
+
+route.get('/teachers',(request,response)=>{
+  response.sendFile('teachers.html',{root:'./public'});
+})
+
+
+route.get('/subjects',(request,response)=>{
+  response.sendFile('subjects.html',{root:'./public'});
+})
+
+route.get('/courses',(request,response)=>{
+  response.sendFile('courses.html',{root:'./public'});
+})
+
+route.get('/courses/:id/batches',(request,response)=>{
+  response.sendFile('batches.html',{root:'./public'});
+})
+
+route.get('/courses/:c_id/batches/:b_id/lectures',(request,response)=>{
+  response.sendFile('lectures.html',{root:'./public'});
+})
+
+
+module.exports=route;
+
